6f1b0d97463ba5e550adc6d2d2cd63bd81661267
huangyu <huangyu@landawang.com>
2019-01-16 14:31:50 +0800

    make fields of MapleRequestOptions optional in types.

--- types/maple-request.d.ts
+++ types/maple-request.d.ts
@@ -6,19 +6,19 @@ import { type } from 'os';
 import { MapleRequestApis } from './maple-request-apis';
 
 export declare class MapleRequestOptions {
-    create: AxiosRequestConfig;
-    apis: MapleRequestApis;
-    interceptors: {
+    create?: AxiosRequestConfig;
+    apis?: MapleRequestApis;
+    interceptors?: {
         request: (config: any) => any;
         requestError: (error: any) => any;
         response: (response: any) => any;
         responseError: (error: any) => any;
     };
-    loading: {
+    loading?: {
         start(): void;
         close(): void;
     };
-    pre: {
+    pre?: {
         request: (data: { [key: string]: any }) => void;
         response: (result: any) => void;
         responseError: (error: any) => void;
