65c0e865eebc02a49099a7e4dd689d15aeed7ddd
huangyu <huangyu@landawang.com>
2019-01-17 09:48:10 +0800

    make types more happier.

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -9,9 +9,9 @@ declare module 'vue/types/vue' {
          * @param {{ [key: string]: any }} data 参数/数据
          * @param {boolean} [loading=false] 开启加载中处理
          * @param {(string | boolean)} [query=false] 开启响应队列
-         * @returns {(string|Promise<{}>)}
+         * @returns 请求结果
          * @memberof Vue
          */
-        $request(key: string, data: { [key: string]: any }, loading?: boolean | number, queue?: string | boolean): Promise<any> & string;
+        $request(key: string, data?: { [key: string]: any }, loading?: boolean | number, queue?: string | boolean): Promise<any> & string;
     }
 }
